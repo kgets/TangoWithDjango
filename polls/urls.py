6bc97eed4fb27263b907376ab7e10324ade8510e
Kelly <kel1161@hotmail.com>
2018-10-30 18:03:56 -0500

    polls added

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	# /polls/
+	path('', views.index, name='index'),
+	# /polls/1
+	path('<int:question_id>/', views.detail, name='detail'),
+	# /polls/1/results/
+	path('<int:question_id>/results', views.results, name='results'),
+	# /polls/1/vote/
+	path('<int:question_id>/vote', views.vote, name='vote'),
+]
