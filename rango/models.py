aa1de451e168dd300044c3941ff8014097cccb0a
Kelly <kel1161@hotmail.com>
2018-10-31 19:30:41 -0500

    c

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.name
@@ -14,6 +16,8 @@ class Page(models.Model):
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
+
 
 	def __str__(self):
 		return self.title
