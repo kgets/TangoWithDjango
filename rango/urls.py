32e8556b45f9e019d5f402ad33488f0bb5ebf426
Kelly <kel1161@hotmail.com>
2018-11-01 11:20:08 -0500

    added categories/<slug> pages linked from top 5 liked categories on index page, as well as, top 5 viewed pages linked directly from index page.

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
 	# /polls/
 	path('', views.index, name='index'),
-	path('about/', views.about, name='about')
+	path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.category, name='category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
